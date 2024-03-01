368482417699e977030596705eab0ed9183758c7
Unic <pashka0ivashen@yandex.ru>
2024-03-01 13:00:44 +0500

    Изменены значения конфигурации

--- sphinx_flyout/__init__.py
+++ sphinx_flyout/__init__.py
@@ -11,21 +11,21 @@ logger = logging.getLogger(__name__)
 def setup(app: Sphinx) -> None:
     app.add_config_value("sphinx_flyout_current_version", "1.0", "html", str)
     app.add_config_value("sphinx_flyout_host", "http://0.0.0.0:8000", "html", str)
-    app.add_config_value("sphinx_flyout_header", "Flyout", "html", str)
-
-    app.add_config_value("sphinx_flyout_downloads", {}, "html", dict)
-    app.add_config_value("sphinx_flyout_gitea", {}, "html", dict)
-    app.add_config_value("sphinx_flyout_versions", {}, "html", dict)
-    app.connect("config-inited", _add_templates)
+    app.add_config_value("sphinx_flyout_repository_link", "", "html", str)
+    app.add_config_value("sphinx_flyout_tags", [], "html", list)
+    app.add_config_value("sphinx_flyout_branches", [], "html", list)
+    app.add_config_value("sphinx_flyout_downloads", [], "html", list)
+    app.connect("config-inited", _add_config_values)
     app.connect("html-page-context", add_flyout_to_context)
 
 
-def _add_templates(app: Sphinx, config: Config):
-    config.templates_path.insert(0, str(Path(__file__).parent / "templates"))
+def _add_config_values(app: Sphinx, config: Config) -> None:
+    config.templates_path.append(str(Path(__file__).parent.parent / "templates"))
+    config.add("sphinx_flyout_header", app.config.project, "html", str)
 
 
 def add_flyout_to_context(app: Sphinx, pagename: str, templatename: str,
-                          context: dict, doctree: Any):
+                          context: dict[str, Any], doctree: Any) -> None:
     if app.config.html_theme != "sphinx_rtd_theme":
         logger.warning(f"Тема {app.config.html_theme} не поддерживается. Пожалуйста, используйте "
                        "'sphinx_rtd_theme'")
@@ -37,13 +37,17 @@ def add_flyout_to_context(app: Sphinx, pagename: str, templatename: str,
     context["downloads"] = _make_links_relate_to_host(
         host, 'download', app.config.sphinx_flyout_downloads
     )
-    context["gitea"] = app.config.sphinx_flyout_gitea
-    context["versions"] = _make_links_relate_to_host(
-        host, 'ver', app.config.sphinx_flyout_versions,
+    context["repository_link"] = f"{host}/{app.config.sphinx_flyout_repository_link}"
+    context["tags"] = _make_links_relate_to_host(
+        host, 'tag', app.config.sphinx_flyout_tags,
+    )
+    context["branches"] = _make_links_relate_to_host(
+        host, 'branch', app.config.sphinx_flyout_branches,
     )
 
 
-def _make_links_relate_to_host(host: str, section: str, links: dict) -> dict:
-    for key, value in links.items():
-        links[key] = f"{host}/{section}/{value}"
-    return links
+def _make_links_relate_to_host(host: str, section: str, links: list[str]) -> dict[str, str]:
+    new_links = {}
+    for link in links:
+        new_links[link] = f"{host}/{section}/{link}"
+    return new_links
